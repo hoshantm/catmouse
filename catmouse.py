c244ebe07a0936496fd01a2b3c1acf72e7043537
Tarik Hoshan <hoshantm@gmail.com>
2019-06-10 19:46:27 +0300

    Use optimization functions to speed up the code.

--- catmouse.py
+++ catmouse.py
@@ -10,21 +10,21 @@ a mouse speed of 1 / CAT_TO_MOUSE_SPEED_RATIO.
 The first two assumptions have no effect on the nature of the solution
 as scaling the cirle up or down will just speed or slow things up. Same goes
 for the cat speed. In fact, only the ratio of cat to mouse velocity has any
-effect on the solution or no solution for that matter.
+effect on the solution or lack of solution for that matter.
 
 @author: tarik
 """
 
 import math
-import operator
+import numpy as np
+from scipy.optimize import minimize
+from scipy.optimize import brentq
 
 CAT_TO_MOUSE_SPEED_RATIO = 4
 ANGLE_INTERVALS = 360
-RATIO_INTERVALS = 400
+
 '''
-ratio: ratio of the mouse distance from the center to the radius. Since we
-assume a radius of one, it comes up to be the same as distance from the
-center.
+distance: distance of the mouse from the center.
 
 alpha: Angle between a line originating from the center and a line orinating
 from the center of the circle and passing through the point M representing 
@@ -35,13 +35,13 @@ align the mouse with the X axis.
 
 Given the above two parameters, calculates the distance between points M and P 
 '''
-def distanceToEdge(ratio, alpha):
+def distanceToEdge(distance, alpha):
     '''
     (cos(alpha), 0) represent the cartesian coordinates of M. We assume here
     that the mouse is located on the X axis.
     P is located at coordinates (cos(alpha), sin(alpha))
     '''
-    return math.sqrt((ratio - math.cos(alpha)) ** 2 + math.sin(alpha) ** 2)
+    return math.sqrt((distance - math.cos(alpha)) ** 2 + math.sin(alpha) ** 2)
 
 '''
 alpha: Angle between a line originating from the center and a line orinating
@@ -65,7 +65,7 @@ def distanceViaEdge(alpha, beta):
     return min(abs(beta - alpha), 2 * math.pi - abs(beta - alpha))
 
 '''
-Given ratio, alpha and beta (see definitions above), calculate the difference
+Given distance, alpha and beta (see definitions above), calculate the difference
 in time between the mouse arrival at point P determined by angle alpha and
 the arrival of the cat to pint P from point C. Note that since the cat is
 CAT_TO_MOUSE_SPEED_RATIO faster than the mouse, it takes the mouse time is
@@ -77,38 +77,52 @@ can escape via a point P determined by alpha. A positive time difference
 indicates a success. Conversly, a zero or negative time difference determines
 a failure. 
 '''
-def diffTimeCatMouse(ratio, alpha, beta):
-    return distanceViaEdge(alpha, beta) - distanceToEdge(ratio, alpha) * CAT_TO_MOUSE_SPEED_RATIO
+def diffTimeCatMouse(distance, alpha, beta):
+    return distanceViaEdge(alpha, beta) - distanceToEdge(distance, alpha) * CAT_TO_MOUSE_SPEED_RATIO
 
 '''
-Given a ratio (mouse location on the X axis. See previous definitions) and
+Given a distance (mouse location on the X axis. See previous definitions) and
 angle beta determining the cat location (see previous definitions) calculate
 alpha that will maximize the arrival time difference at point P determined
 by alpha, and the arrival of the cat to point P.
-A positive number indicates that the initial conditions of ratio and beta
+A positive number indicates that the initial conditions of distance and beta
 guarantees the mouse escaping in a straight line via point P.
 What is couterintuitive here is that the mouse could swim along a line not
 necessary aligned with the X axis and shave off some time.
 '''
-def maxDiffTimeCatMouse(ratio, beta):
-    alphas = [math.pi * 2 / ANGLE_INTERVALS * i for i in range(ANGLE_INTERVALS+1)]
-    distances = [diffTimeCatMouse(ratio, alpha, beta) for alpha in alphas]
-    max_index, max_distance = max(enumerate(distances), key=operator.itemgetter(1))
-    max_alpha = alphas[max_index]
-    return max_alpha, max_distance
+def maxDiffTimeCatMouse(distance, beta):
+    f1 = lambda alpha : alpha - beta + CAT_TO_MOUSE_SPEED_RATIO * math.sqrt((distance - math.cos(alpha)) ** 2 + math.sin(alpha) ** 2)
+    x0 = np.array([beta - math.pi / 2])
+    bounds = [(beta - math.pi, beta)]
+    res = minimize(f1, x0, method='L-BFGS-B', bounds=bounds)
+    argmax1 = res['x'][0]
+    if argmax1 < 0:
+        argmax1 = math.pi * 2 + argmax1
+    max1 = -res['fun'][0]    
+    
+    f2 = lambda alpha : -alpha + beta - 2 * math.pi + CAT_TO_MOUSE_SPEED_RATIO * math.sqrt((distance - math.cos(alpha)) ** 2 + math.sin(alpha) ** 2)
+    x0 = np.array([0])
+    bounds = [(beta - 2 * math.pi, beta - math.pi)]
+    res = minimize(f2, x0, method='L-BFGS-B', bounds=bounds)
+    argmax2 = res['x'][0]
+    if argmax2 < 0:
+        argmax2 = math.pi * 2 + argmax2
+    max2 = -res['fun'][0]    
+       
+    if max1 >= max2:
+        return argmax1, max1
+    else:
+        return argmax2, max2
 
 '''
 Given a given angle beta as previously defined, calculate the minimum distance
 from the center to ensure the mouse escape. Meaning that the smaller the angle,
 the closer the mouse should be to the edge.
 '''
-def zeroDistanceRatioLimit(beta):
-    i = 0
-    interval_size = 1.0 / RATIO_INTERVALS
-    while (maxDiffTimeCatMouse(i * interval_size, beta)[1] < 0):
-        i+=1
-        
-    return i * interval_size
+def minimumEscapeDistance(beta):
+    f = lambda distance : maxDiffTimeCatMouse(distance, beta)[1]
+    x = brentq(f, 0, 1)
+    return x
 
 '''
 Calculate a list of points defining a boundary between two regions. All the
@@ -118,9 +132,5 @@ has a guaranteed escape. The bottom region are failure start positions.
 def getBoundary():
     interval_size = math.pi * 2 / ANGLE_INTERVALS
     betas = [i * interval_size for i in range(ANGLE_INTERVALS + 1)]
-    ratios = [zeroDistanceRatioLimit(beta) for beta in betas]
-    return betas, ratios
-
-
-
-    
+    distances = [minimumEscapeDistance(beta) for beta in betas]
+    return betas, distances
