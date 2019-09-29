ba221b4f06039f5cfca958aa997cd64d6c7e88a7
Tarik Hoshan <hoshantm@gmail.com>
2019-09-29 15:48:51 +0300

    Major update with improved comments

--- catmouse.py
+++ catmouse.py
@@ -22,91 +22,74 @@ from scipy.optimize import brentq
 
 CAT_TO_MOUSE_SPEED_RATIO = 4
 ANGLE_INTERVALS = 360
+DEBUG = False
 
 '''
-distance: distance of the mouse from the center.
-
-alpha: Angle between a line originating from the center and a line orinating
-from the center of the circle and passing through the point M representing 
-the mouse. The first line determines a point P through which it itersects
-with the circle. We assume here that M is located on the X axis. This
-assumption does not affect anything as we can rotate the entire world to 
-align the mouse with the X axis.
-
-Given the above two parameters, calculates the distance between points M and P 
+alpha, distance: Polar coordinates of point M representing the mouse.
+beta, 1: Polar coordinates of an arbitrary point P on the circle.
+Return value: Distance between M and P
 '''
-def distanceToEdge(distance, alpha):
+def distanceToEdge(distance, alpha, beta):
     '''
     (cos(alpha), 0) represent the cartesian coordinates of M. We assume here
     that the mouse is located on the X axis.
     P is located at coordinates (cos(alpha), sin(alpha))
     '''
-    return math.sqrt((distance - math.cos(alpha)) ** 2 + math.sin(alpha) ** 2)
+    return math.sqrt((distance - math.cos(alpha - beta)) ** 2 + math.sin(alpha - beta) ** 2)
 
 '''
-alpha: Angle between a line originating from the center and a line orinating
-from the center of the circle and passing through the point M representing 
-the mouse. The first line determines a point P through which it itersects
-with the circle. We assume here that M is located on the X axis. This
-assumption does not affect anything as we can rotate the entire world to 
-align the mouse with the X axis.
+beta, 1: Coordinates of an arbitrary point P on the circle.
+0, 1: Position of the cat. This assumption has no effect as we can always
+use a rotation transform to position the cat at 0, 1.
 
-beta: Angle between a line originating from the center and intersecting the
-circle at point C representing the cat, and the X axis.
-
-Both alpha and beta are measured in radian clockwise from the X axis. That
-makes it trivial to measure the arc lengths between C and P. The word "lengths"
-is plural as we can calculate the lengths of both arcs determined by C and P.
-
-We define here the distance of C to M as the length of the arc with the minimum
+We define here the distance of C to P as the length of the arc with the minimum
 distance. Alternatively, the arc of lenth <= PI.
 '''
-def distanceViaEdge(alpha, beta):
-    return min(abs(beta - alpha), 2 * math.pi - abs(beta - alpha))
+def distanceViaEdge(beta):
+    return min(abs(beta), 2 * math.pi - abs(beta))
 
 '''
-Given distance, alpha and beta (see definitions above), calculate the difference
-in time between the mouse arrival at point P determined by angle alpha and
-the arrival of the cat to pint P from point C. Note that since the cat is
-CAT_TO_MOUSE_SPEED_RATIO faster than the mouse, it takes the mouse time is
-multiplied by CAT_TO_MOUSE_SPEED_RATIO. Alternatively, we could have divided
-the cat time by CAT_TO_MOUSE_SPEED_RATIO. Since the cat velocity is assumed
-to be one, we do not bother dividing by one to get the time.
+alpha, distance: Polar coordinates of point M representing the mouse.
+beta, 1: Polar coordinates of an arbitrary point P on the circle.
+Return: Difference in time between the cat arrival at point P traveling on
+the edge of the circle and the arrival of the mouse at point P traveling through
+a straight line from point M.
+Cat velocity: 1
+Mouse velocity: 1 / CAT_TO_MOUSE_SPEED_RATIO
+
+Since the cat velocity is assumed to be one, we do not bother dividing by one to
+get the cat travel time.
+
 The idea of this function is to find out if a mouse at a given location
 can escape via a point P determined by alpha. A positive time difference
 indicates a success. Conversly, a zero or negative time difference determines
 a failure. 
 '''
 def diffTimeCatMouse(distance, alpha, beta):
-    return distanceViaEdge(alpha, beta) - distanceToEdge(distance, alpha) * CAT_TO_MOUSE_SPEED_RATIO
+    return distanceViaEdge(beta) - distanceToEdge(distance, alpha, beta) * CAT_TO_MOUSE_SPEED_RATIO
 
 '''
-Given a distance (mouse location on the X axis. See previous definitions) and
-angle beta determining the cat location (see previous definitions) calculate
-alpha that will maximize the arrival time difference at point P determined
-by alpha, and the arrival of the cat to point P.
-A positive number indicates that the initial conditions of distance and beta
-guarantees the mouse escaping in a straight line via point P.
-What is couterintuitive here is that the mouse could swim along a line not
-necessary aligned with the X axis and shave off some time.
+alpha, distance: Polar coordinates of point M representing the mouse
+0, 1: Position of the cat. This assumption has no effect as we can always
+use a rotation transform to position the cat at 0, 1.
+
+Determine the angle of point with polar coordinates (beta, 1) that maximizes
+the difference in arrival time between the mouse and the cat. Note that the
+shortest path from the mouse to the edge of the circle in not optimum.
 '''
-def maxDiffTimeCatMouse(distance, beta):
-    f1 = lambda alpha : alpha - beta + CAT_TO_MOUSE_SPEED_RATIO * math.sqrt((distance - math.cos(alpha)) ** 2 + math.sin(alpha) ** 2)
-    x0 = np.array([beta - math.pi / 2])
-    bounds = [(beta - math.pi, beta)]
+def maxDiffTimeCatMouse(distance, alpha):
+    f1 = lambda beta : -(beta - CAT_TO_MOUSE_SPEED_RATIO * distanceToEdge(distance, alpha, beta))
+    x0 = np.array([0])
+    bounds = [(0, math.pi)]
     res = minimize(f1, x0, method='L-BFGS-B', bounds=bounds)
     argmax1 = res['x'][0]
-    if argmax1 < 0:
-        argmax1 = math.pi * 2 + argmax1
     max1 = -res['fun'][0]    
     
-    f2 = lambda alpha : -alpha + beta - 2 * math.pi + CAT_TO_MOUSE_SPEED_RATIO * math.sqrt((distance - math.cos(alpha)) ** 2 + math.sin(alpha) ** 2)
-    x0 = np.array([0])
-    bounds = [(beta - 2 * math.pi, beta - math.pi)]
+    f2 = lambda beta : -((2 * math.pi - beta) - CAT_TO_MOUSE_SPEED_RATIO * distanceToEdge(distance, alpha, beta))
+    x0 = np.array([2 * math.pi])
+    bounds = [(math.pi, 2 * math.pi)]
     res = minimize(f2, x0, method='L-BFGS-B', bounds=bounds)
     argmax2 = res['x'][0]
-    if argmax2 < 0:
-        argmax2 = math.pi * 2 + argmax2
     max2 = -res['fun'][0]    
        
     if max1 >= max2:
@@ -115,14 +98,15 @@ def maxDiffTimeCatMouse(distance, beta):
         return argmax2, max2
 
 '''
-Given a given angle beta as previously defined, calculate the minimum distance
-from the center to ensure the mouse escape. Meaning that the smaller the angle,
-the closer the mouse should be to the edge.
+alpha, distance: Polar coordinates of point M representing the mouse.
+0, 1: Polar coordinates of cat
+Given alpha, determine the minimum distance of the mouse from the center
+to allow for an escape.
 '''
-def minimumEscapeDistance(beta):
-    f = lambda distance : maxDiffTimeCatMouse(distance, beta)[1]
-    x = brentq(f, 0, 1)
-    return x
+def minimumEscapeDistance(alpha):
+    f = lambda distance : maxDiffTimeCatMouse(distance, alpha)[1]
+    distance = brentq(f, 0, 1)
+    return distance
 
 '''
 Calculate a list of points defining a boundary between two regions. All the
@@ -133,4 +117,7 @@ def getBoundary():
     interval_size = math.pi * 2 / ANGLE_INTERVALS
     betas = [i * interval_size for i in range(ANGLE_INTERVALS + 1)]
     distances = [minimumEscapeDistance(beta) for beta in betas]
+    if DEBUG:
+        for i, beta in enumerate(betas):
+            print (i, beta, distances[i])
     return betas, distances
