62514d260492e1693679c2e5e4eb8eab6781d1cc
Tarik Hoshan <hoshantm@gmail.com>
2020-07-29 19:52:01 +0300

    Improved comments

--- python/spyral_path.py
+++ python/spyral_path.py
@@ -3,7 +3,7 @@
 """
 Created on Fri Jun  7 05:00:46 2019
 
-Plot of the spiral path representing the optimal route towards the edge of the
+Plot of the circular path representing the optimal route towards the edge of the
 circle while staying diametrically opposite to the cat position. The assumed
 initial positions are center for the mouse and angle PI for the cat. The mouse
 original velocity vector is 1 / CAT_TO_MOUSE_SPEED_RATIO along the positive
@@ -18,40 +18,40 @@ instantly and follow a mirror image of the remainder of the escape path
 in relation with the radius passing by the current mouse location.
 
 The path is the solution of the following diffential equation:
-sqrt(r'^2 + r^2) = 1/4
-r' = sqrt(1/16 - r^2)
+sqrt(r'^2 + r^2) = 1/CAT_TO_MOUSE_SPEED_RATIO
+r' = sqrt(1/CAT_TO_MOUSE_SPEED_RATIO^2 - r^2)
 Using https://www.symbolab.com ODE solver, we get:
-r(t) = sin(t + c1) / 4
+r(t) = sin(t + c1) / CAT_TO_MOUSE_SPEED_RATIO
 c1 represents the original time, but also the angle theta since the angle
 velocity is constant and equal to one radian per second. We set c1 to zero.
 Notes:
-- For a CAT_TO_MOUSE_SPEED_RATIO different than 4, use
-  1/CAT_TO_MOUSE_SPEED_RATIO instead of 1/4 and 1/CAT_TO_MOUSE_SPEED_RATIO^2
-  instead of 1/16.
+- CAT_TO_MOUSE_SPEED_RATIO can be set to any value different than 4.
 - With CAT_TO_MOUSE_SPEED_RATIO = 4, past t=PI/2, the mouse will start
-  spiraling back towards the center. It's up to us human to determine the
+  circling back towards the center. It's up to us human to determine the
   boundaries of the mathematical solution.
-- When transformed from polar to cartesian coordinates, it seems that the
-  path is simply a half circle of radius 1/4 and center (0, 1/8). I did
-  not verify this though.
+- When transformed from polar to cartesian coordinates, the
+  path is simply a half circle of radius 1/CAT_TO_MOUSE_SPEED_RATIO and 
+  center (0, 1/CAT_TO_MOUSE_SPEED_RATIO/2). I did not verify this though.
 
-@author: tarik
+@author: Tarik Hoshan
 """
 
 import math
 import matplotlib.pyplot as plt
 
+CAT_TO_MOUSE_SPEED_RATIO = 4.0
+
 INTERVALS = 1000
 inc = math.pi / 2 / INTERVALS
 thetas = [i * inc for i in range(INTERVALS + 1)]
-rs = [math.sin(theta) / 4 for theta in thetas]
+rs = [math.sin(theta) / CAT_TO_MOUSE_SPEED_RATIO for theta in thetas]
 
 ax = plt.subplot(111, projection='polar')
 ax.plot(thetas, rs)
 
 # draw circle
 thetas2 = [2 * math.pi / INTERVALS * i for i in range(INTERVALS+1)]
-rs2 = [1.0 / 4.0 for i in range(INTERVALS+1)]
+rs2 = [1.0 / CAT_TO_MOUSE_SPEED_RATIO for i in range(INTERVALS+1)]
 ax.plot(thetas2, rs2)
 
 ax.set_rmax(1)
@@ -62,19 +62,19 @@ ax.grid(True)
 plt.show()
 
 '''
-Verify that the velocity is constant = 1/4 regardless of the
+Verify that the velocity is constant = 1/CAT_TO_MOUSE_SPEED_RATIO regardless of the
 time between 0 and PI/2. The value of frac can be modified
 to any value between 0 and 1, speed should always round up
-to 0.25
+to the correct value of 1 / CAT_TO_MOUSE_SPEED_RATIO
 '''
 frac = 0.3
 t = math.pi / 2 * frac
 theta1 = t
-r1 = math.sin(theta1) / 4
+r1 = math.sin(theta1) / CAT_TO_MOUSE_SPEED_RATIO
 
 epsilon = 0.00001
 theta2 = t + epsilon
-r2 = math.sin(theta2) / 4
+r2 = math.sin(theta2) / CAT_TO_MOUSE_SPEED_RATIO
 
 x1 = math.cos(theta1) * r1
 y1 = math.sin(theta1) * r1
@@ -84,4 +84,5 @@ y2 = math.sin(theta2) * r2
 
 speed = math.sqrt((x2 - x1)**2 + (y2 - y1)**2) / epsilon
 speed = round(speed, 10)
-print(speed)
+correct_speed = round(1/CAT_TO_MOUSE_SPEED_RATIO, 10)
+print(speed, correct_speed)
